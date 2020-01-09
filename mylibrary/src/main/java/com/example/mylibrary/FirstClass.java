b530c4441e1bc1fcc09173fab13f35c3eabac997
udit <itgetjobs4@gmail.com>
2020-01-09 13:10:42 +0530

    initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/FirstClass.java
@@ -0,0 +1,16 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+public class FirstClass {
+
+    public static void main(String[] args){
+
+    }
+
+    public static void myFun(){
+
+        System.out.println("Yes it is assessible");
+        Log.i("HeyCheck","Yes it is assessible");
+    }
+}
